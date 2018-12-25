b943685eb18081280bf4b9baaddfd5a4f8e460c1
Hien Le <hienlegiuse8@yahoo.com>
2018-12-25 11:32:47 -0800

    second

--- glvnt/Model/Utils.cs
+++ glvnt/Model/Utils.cs
@@ -10,7 +10,7 @@ namespace glvnt.Model
     public class Utils
     {
         public static string ERROR = "error";
-        public static string CONNSTRING = "server=146.66.103.232;database=leohien4_glvnt;uid=leohien4_tnnt;pwd=NamThaiThieuNhi18;Convert Zero Datetime=True;";
+        public static string CONNSTRING = "server=146.66.103.232;database=leohien4_glvnt;uid=leohien4_tnnt;pwd=testingtesting;Convert Zero Datetime=True;";
         public static void showError(string ErrMessage)
         {
             MessageBox.Show("Exception Error: " + ErrMessage, Utils.ERROR, MessageBoxButton.OK, MessageBoxImage.Error);
